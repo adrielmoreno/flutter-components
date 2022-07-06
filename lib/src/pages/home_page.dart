49ba8517c28ab202eee99944e463528689ed82a7
MorenoCreativo <adrielmoreno@hotmail.com>
2022-07-06 12:12:09 +0200

    List loaded from JSON using Providers

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:components/src/providers/menu_provider.dart';
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -12,19 +14,35 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _list() {
-    return ListView(
-      children: _listItems(),
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      initialData: [],
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+        return ListView(
+          children: _listItems(snapshot.data),
+        );
+      },
     );
   }
 
-  List<Widget> _listItems() {
-    return [
-      const ListTile(title: Text('Hola Mundo')),
-      const Divider(),
-      const ListTile(title: Text('Hola Mundo')),
-      const Divider(),
-      const ListTile(title: Text('Hola Mundo')),
-      const Divider(),
-    ];
+  List<Widget> _listItems(List<dynamic>? data) {
+    final List<Widget> options = [];
+
+    data?.forEach((opt) {
+      final widgetTemp = ListTile(
+        title: Text(opt['texto']),
+        leading: const Icon(Icons.account_box,
+            color: Color.fromRGBO(108, 181, 241, 1)),
+        trailing: const Icon(Icons.arrow_circle_right_outlined,
+            color: Color(0xFF6CB5F1)),
+        onTap: () {},
+      );
+
+      options
+        ..add(widgetTemp)
+        ..add(const Divider());
+    });
+
+    return options;
   }
 }

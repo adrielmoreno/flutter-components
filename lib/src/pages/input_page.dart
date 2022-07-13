71b67e38b02c0b5711c81d1ffd388c6d532d8de6
MorenoCreativo <adrielmoreno@hotmail.com>
2022-07-13 14:03:10 +0200

    Created Form

--- lib/src/pages/input_page.dart
+++ lib/src/pages/input_page.dart
@@ -9,6 +9,9 @@ class InputPage extends StatefulWidget {
 
 class _InputPageState extends State<InputPage> {
   String _nombre = '';
+  String _email = '';
+  String _fecha = '';
+  final TextEditingController _inputDateController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +22,12 @@ class _InputPageState extends State<InputPage> {
         padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
         children: <Widget>[
           _crearInput(),
+          const Divider(),
+          _crearEmail(),
+          const Divider(),
+          _crearPassword(),
+          const Divider(),
+          _crearFecha(context),
           const Divider(),
           _crearPersona(),
         ],
@@ -52,7 +61,81 @@ class _InputPageState extends State<InputPage> {
 
   Widget _crearPersona() {
     return ListTile(
-      title: Text('El nombre es $_nombre'),
+      title: Text('Nombre $_nombre'),
+      subtitle: Text('Email: $_email'),
     );
+  }
+
+  Widget _crearEmail() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: const InputDecoration(
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(20.0))),
+          hintText: 'Email',
+          label: Text('Email:'),
+          suffix: Icon(
+            Icons.alternate_email,
+            color: Colors.blue,
+          ),
+          icon: Icon(Icons.mark_email_unread_outlined)),
+      onChanged: (value) => setState(() {
+        _email = value;
+      }),
+    );
+  }
+
+  Widget _crearPassword() {
+    return TextFormField(
+      obscureText: true,
+      keyboardType: TextInputType.visiblePassword,
+      decoration: const InputDecoration(
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(20.0))),
+          hintText: 'Password',
+          label: Text('Password:'),
+          suffix: Icon(
+            Icons.password,
+            color: Colors.blue,
+          ),
+          icon: Icon(Icons.lock_outline)),
+      onChanged: (value) {},
+    );
+  }
+
+  Widget _crearFecha(BuildContext context) {
+    return TextFormField(
+      enableInteractiveSelection: false,
+      controller: _inputDateController,
+      decoration: const InputDecoration(
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(20.0))),
+          hintText: 'Fecha de nacimiento',
+          label: Text('Fecha de nacimiento:'),
+          suffix: Icon(
+            Icons.date_range,
+            color: Colors.blue,
+          ),
+          icon: Icon(Icons.calendar_month)),
+      onTap: () {
+        FocusScope.of(context).requestFocus(FocusNode());
+        _selectDate(context);
+      },
+    );
+  }
+
+  _selectDate(BuildContext context) async {
+    DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(2020),
+        lastDate: DateTime(2023));
+
+    if (picked != null) {
+      setState(() {
+        _fecha = picked.toString();
+        _inputDateController.text = _fecha;
+      });
+    }
   }
 }

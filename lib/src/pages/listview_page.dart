e4dcf9358758dc2a36d623927ecb6bca374c693c
MorenoCreativo <adrielmoreno@hotmail.com>
2022-07-14 10:06:54 +0200

    Created infiniteScroll

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -8,7 +8,24 @@ class ListPage extends StatefulWidget {
 }
 
 class _ListPageState extends State<ListPage> {
-  final List<int> _listaNumeros = [1, 2, 3, 4, 5];
+  final ScrollController _scrollController = ScrollController();
+
+  final List<int> _listaNumeros = [];
+  int _ultimoItem = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _agregar10();
+
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        _agregar10();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,6 +38,7 @@ class _ListPageState extends State<ListPage> {
 
   Widget _crearLista() {
     return ListView.builder(
+      controller: _scrollController,
       itemCount: _listaNumeros.length,
       itemBuilder: (BuildContext context, int index) {
         final imagen = _listaNumeros[index];
@@ -30,5 +48,13 @@ class _ListPageState extends State<ListPage> {
                 NetworkImage('https://picsum.photos/500/300?random=$imagen'));
       },
     );
+  }
+
+  void _agregar10() {
+    for (var i = 0; i < 10; i++) {
+      _ultimoItem++;
+      _listaNumeros.add(_ultimoItem);
+    }
+    setState(() {});
   }
 }
